620bf8eb2ab3a78c2635a3f80a44e6b94bfee2d9
Unknown <LShumate039@west-mec.org>
2019-02-25 18:20:28 -0700

    update 2/25
    put in an if and added new things for leap years

--- lht_calendar.js
+++ lht_calendar.js
@@ -38,6 +38,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
+      calendarHTML += calWeekdayRow();
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -57,4 +58,33 @@ function calCaption(calDate) {
 
       return "<caption>" + monthName[thisMonth] + " " +
             thisYear + "</caption>";
+}
+// function to write a table row of weekday abbreviations
+function calWeekdayRow() {
+      var dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      var rowHTML = "<tr>";
+      //look through the day name arry 
+      for (var i = 0; i < dayName.length; i++) {
+            rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+      }
+      rowHTML += "</tr>";
+      return rowHTML;
+}
+
+//funtion to calculate the number of days in a month
+function daysInMonth(calDate) {
+      //array of days in each month 
+      var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+      //extract the four digit year and month 
+      var thisYear = calDate.getFullYear();
+      var thisMonth = calDate.getMonth();
+
+      //revise the days in feb for leap years 
+      if (thisYear % 4 === 0) {
+            dayCount[1] = 29;
+      }
+
+      //return the number of days for the current month 
+      return dayCount[thisMonth];
 }
